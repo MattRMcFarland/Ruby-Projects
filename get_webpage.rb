ae3e8dc4c5021b46353e5d47c096a1c065128b5d
Matt McFarland <matthew.r.mcfarland.16@dartmouth.edu>
2015-03-15 13:13:50 -0400

    downloaded first html page

--- get_webpage.rb
+++ get_webpage.rb
@@ -1,21 +1,36 @@
 #!/usr/bin/env ruby
 
-# Given a list of url arguments, get html and saves text in ./html_files directory
+# Given a list of url arguments, gets html and saves text in ./html_files directory
+# Also collects a list of urls found in html
+# 
 # USAGE:
-#	./get_webpage.rb [URLS]
+#	./get_webpage.rb [SEED URL]
 
 require 'nokogiri'
 require 'open-uri'
 
-Dir::mkdir( "html_files", 700) unless Dir::exist?( "html_files" )
+FileDir = "./html_files"
 
-page = open('http://en.wikipedia.org/wiki/Puppy').read
-puts page.length
-puts page
+Dir::mkdir( FileDir, 0711) unless Dir::exist?( FileDir )
+
+#page = open('http://en.wikipedia.org/wiki/Puppy').read
+#puts page.length
+#puts page
+
+def save( url )
+	puts FileDir
+end
+
+i = 1
 
 # for each argument, get html text
-#ARGV.each do |a|
-#	puts open(a).read
-#end
+ARGV.each do |a|
+	if page = open(a).read
+		puts "#{FileDir}/" + a
+		File::open( "#{FileDir}/" + i.to_s, "w") { |file| file.write( page ) }	
+	else
+		puts "Could not open #{a}"
+	end
+end
 
 # save to file
